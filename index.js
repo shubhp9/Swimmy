e11622492afc937648151a3e087824f00bc3fcae
Shubh <70544266+shubhp9@users.noreply.github.com>
2020-09-18 19:01:33 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,54 +1,55 @@
 const Discord = require('discord.js');
-const fs =  require('fs');
 const config = require('./config.json');
-const { Kayn, REGIONS } = require('kayn');
+
+let LeagueAPI = require('leagueapiwrapper');
+LeagueAPI = new LeagueAPI(config.riot_api_key, Region.NA);
 
 const client = new Discord.Client();
-client.commands = new Discord.Collection();
-
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles)    {
-    const command = require(`./commands/${file}`);
-
-    client.commands.set(command.name, command);
-}
-
-const kayn = Kayn(config.riot_api_key)({
-    region: REGIONS.NORTH_AMERICA,
-    apiURLPrefix: 'https://%s.api.riotgames.com',
-    locale: 'en_US',
-    debugOptions: {
-        isEnabled: true,
-        showKey: false,
-    },
-    requestOptions: {
-        shouldRetry: true,
-        numberOfRetriesBeforeAbort: 3,
-        delayBeforeRetry: 1000,
-        burst: false,
-        shouldExitOn403: false,
-    },
-    cacheOptions: {
-        cache: null,
-        timeToLives: {
-            useDefault: false,
-            byGroup: {},
-            byMethod: {},
-        },
-    },
-});
 
 client.on('ready', () => {
     console.log('This bot is online!');
+
 })
 
-client.on('message', msg =>{
+client.on('message', message =>{
 
     const args = message.content.slice(config.prefix.length).split(/ +/);
-    const commandName = args.shift().toLowerCase();
 
-    const command = client.commands.get(commandName);
+    if (args[0] == "summoner")  {
+        LeagueAPI.getSummonerByName(args[1])
+        .then(function(accountInfo) {
+        message.channel.send(accountInfo.name.toString() + "'s account level is: " + accountInfo.summonerLevel.toString());
+	    console.log(accountInfo);
+        })
+        .catch(console.log);
+    }
+
+    else if (args[0] == "current"){
+
+        LeagueAPI.getSummonerByName(args[1])
+	    .then(function(accountObject) {
+		    // Gets active games. Will return 404 if not currently in an active game
+		    return LeagueAPI.getActiveGames(accountObject);
+	    })
+	    .then(function(activeGames) { 
+            console.log(activeGames);
+            var players = "";
+            for (var i = 0; i < activeGames.participants.length; i++)   {
+                if (i == 0) {
+                    players = "On " + args[1] + "'s team:";
+                }
+                if (i == 5) {
+                    players = players + "\n\nOn enemy team:";
+                }
+                players = players + "\n" + activeGames.participants[i].summonerName.toString();
+            }
+            message.channel.send(players);
+            
+	    })
+	    .catch(console.log);
+    }
+
+
 
 })
 
